f88291725bbaef82d3618c5c9118d307933a3492
Subfloor6454 <116109421+Subfloor6454@users.noreply.github.com>
2023-01-11 01:28:45 +0000

    refactor: EthTransferFailedError string argument

--- src/helpers/HelpersTransferEthOrERC20.sol
+++ src/helpers/HelpersTransferEthOrERC20.sol
@@ -5,7 +5,7 @@ import { Constants } from "src/constants/Constants.sol";
 import { HelpersERC20 } from "src/helpers/HelpersERC20.sol";
 
 library HelpersTransferEthOrERC20 {
-    error EthTransferFailedError(bytes reason);
+    error EthTransferFailedError(string reason);
 
     // Reentrant. Use checks-effects-interactions pattern.
     function transfer(address token_, address receiver_, uint256 amount_) internal {
@@ -13,7 +13,7 @@ library HelpersTransferEthOrERC20 {
             HelpersERC20.transfer(token_, receiver_, amount_);
         } else {
             (bool success, bytes memory reason) = receiver_.call{value: amount_}("");
-            if (!success) revert EthTransferFailedError(reason);
+            if (!success) revert EthTransferFailedError(string(reason));
         }
     }
 }
