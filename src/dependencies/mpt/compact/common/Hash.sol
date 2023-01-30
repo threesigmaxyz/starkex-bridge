689c1dac9c66ad612ff8c1058f2782594b406d70
0x73696d616f <116109421+0x73696d616f@users.noreply.github.com>
2023-01-30 08:35:56 +0000

    feat: batch verify from parity tech

---
+++ src/dependencies/mpt/compact/common/Hash.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Memory.sol";
+
+library Hash {
+
+    function hash(bytes memory src) internal view returns (bytes memory des) {
+        return Memory.toBytes(keccak256(src));
+    }
+}
