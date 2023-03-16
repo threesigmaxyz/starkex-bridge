b32cea49a04b415dc7a8c14a042b4759721df797
Eduardo Catarino <eduardo.catarino@bee-eng.pt>
2023-03-16 16:13:33 +0000

    feat: add celer bridge

---
+++ src/interfaces/interoperability/ICelerReceptor.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ICelerReceptor {
+
+    enum ExecutionStatus {
+        Fail, // execution failed, finalized
+        Success, // execution succeeded, finalized
+        Retry // execution rejected, can retry later
+    }
+
+    /**
+     * @notice Emitted when the bridge address is set.
+     * @param bridge The bridge address.
+     */
+    event LogSetBridge(address indexed bridge);
+
+    /**
+     * @notice Emitted when an outdated root is received (lz messages sent in the wrong order).
+     * @param orderRoot The order root.
+     * @param nonce The nonce of the message.
+     */
+    event LogOutdatedRootReceived(uint256 indexed orderRoot, uint64 indexed nonce);
+
+    /**
+     * @notice Emitted when the order root of the bridge is updated.
+     * @param orderRoot The order root.
+     */
+    event LogOrderRootUpdate(uint256 indexed orderRoot);
+
+    /**
+     * @notice Emitted when an order root is received.
+     * @param orderRoot The order root.
+     */
+    event LogRootReceived(uint256 indexed orderRoot);
+
+    /// @notice Emitted when the interoperability role in the bridge is accepted.
+    event LogBridgeRoleAccepted();
+
+    error ZeroBridgeAddressError();
+    error ZeroCelerAddressError();
+    error InvalidCallerError();
+    error VerificationFailError(string reason_);
+    error InvalidEmitterError();
+    error AlreadyProcessedError();
+    error NotIntendedRecipientError();
+
+    /// @notice Accepts the pending bridge role.
+    function acceptBridgeRole() external;
+
+    /**
+     * @notice The owner sets the root of the order tree.
+     *         This adds another layer of security, stopping celer from sending any message.
+     */
+    function setOrderRoot() external;
+
+    /**
+     * @notice Called by MessageBus to execute a message
+     * @param emitterAddress_ The address of the source app contract
+     * @param emitterChainId_ The source chain ID where the transfer is originated from
+     * @param message_ Arbitrary message bytes originated from and encoded by the source app contract
+     * @param executor_ Address who called the MessageBus execution function
+     */
+    function executeMessage(
+        address emitterAddress_,
+        uint64 emitterChainId_,
+        bytes calldata message_,
+        address executor_
+    ) external payable returns (ExecutionStatus);
+}
---
+++ src/interoperability/CelerReceptor.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { LibAccessControl } from "src/libraries/LibAccessControl.sol";
+import { IAccessControlFacet } from "src/interfaces/facets/IAccessControlFacet.sol";
+import { IStateFacet } from "src/interfaces/facets/IStateFacet.sol";
+import { IMessageBus } from "src/dependencies/celer/interfaces/IMessageBus.sol";
+import { ICelerReceptor } from "src/interfaces/interoperability/ICelerReceptor.sol";
+import { CelerBase } from "src/interoperability/celer/CelerBase.sol";
+
+contract CelerReceptor is ICelerReceptor, CelerBase {
+    /// @notice Address of the _bridge.
+    address private immutable _bridge;
+
+    /// @notice Last nonce received. Useful to ignore outdated received roots.
+    uint256 private _lastNonce;
+
+    uint256 private _orderRoot;
+
+    mapping(bytes32 => bool) public processedMessages;
+
+    constructor(address messageBus_, address bridge_) CelerBase(messageBus_) {
+        if (messageBus_ == address(0)) revert ZeroCelerAddressError();
+        if (bridge_ == address(0)) revert ZeroBridgeAddressError();
+
+        _bridge = bridge_;
+        emit LogSetBridge(bridge_);
+    }
+
+    /// @inheritdoc ICelerReceptor
+    function acceptBridgeRole() public override {
+        IAccessControlFacet(_bridge).acceptRole(LibAccessControl.INTEROPERABILITY_CONTRACT_ROLE);
+        emit LogBridgeRoleAccepted();
+    }
+
+    /// @inheritdoc ICelerReceptor
+    function setOrderRoot() external override onlyOwner {
+        uint256 orderRoot_ = _orderRoot;
+        IStateFacet(_bridge).setOrderRoot(orderRoot_);
+        emit LogOrderRootUpdate(orderRoot_);
+    }
+
+    /// @inheritdoc ICelerReceptor
+    function executeMessage(address emitterAddress_, uint64 emitterChainId_, bytes calldata message_, address ) public payable override returns (ExecutionStatus) {
+        // Function must be called only by the relayer for security.
+        if (_msgSender() != address(messageBus)) revert InvalidCallerError();
+
+        // Ensure the emitterAddress of this is a trusted address.
+        bytes memory trustedRemote_ = trustedRemoteLookup[uint16(emitterChainId_)];
+        if (keccak256(trustedRemote_) != keccak256(abi.encodePacked(emitterAddress_))) revert InvalidEmitterError();
+
+        (uint256 orderRoot_, bytes memory intendedRecipient_) = abi.decode(message_, (uint256, bytes));
+
+        // Check that the contract which is processing is the intendedRecipient
+        if (keccak256(intendedRecipient_) != keccak256(abi.encodePacked(address(this)))) {
+            revert NotIntendedRecipientError();
+        }
+
+        _orderRoot = orderRoot_;
+        emit LogRootReceived(orderRoot_);
+
+        return ExecutionStatus.Success;
+    }
+}
